0d525f602446f01fc59219aded7b076575704c6b
LucasVillaume <lvillaume17@gmail.com>
2020-12-02 19:26:52 +0100

    Création de la super classe Plateforme

---
+++ Plateforme.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Plateforme.hpp"
+
+Plateforme::Plateforme(Point deb, Point fin): hautGauche(deb), basDroite(fin){
+
+}
+
+Plateforme::Plateforme(int x, int y, int x2, int y2): hautGauche(Point(x,y)), basDroite(Point(x2,y2)){
+
+}
---
+++ Plateforme.hpp
@@ -0,0 +1,23 @@
+#ifndef DEF_PLATEFORME
+#define DEF_PLATEFORME
+
+#include <SFML/Graphics.hpp>
+#include "Point.hpp"
+
+class Plateforme{
+
+	public:
+	Plateforme(Point deb, Point fin);
+	Plateforme(int x, int y, int x2, int y2);
+	void casser();
+	void reparer();
+	
+	protected:
+	Point hautGauche;
+	Point basDroite;
+	sf::Texture texturePlateforme;
+	sf::Sprite sprPlateforme;
+
+};
+
+#endif
